30e15c4649c302bb307983ecf455f0c2cd0d1f9c
Ch0-ppA <darth.j@yandex.ru>
2021-10-14 14:22:44 +0500

    finish working

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ function showTab() {
     document.getElementById('sect_'+tabNumber).style.display='block';
 }
 
+function progressView(){
+    let diagramBox = document.querySelectorAll('.diagram.progress');
+    diagramBox.forEach((box) => {
+        let deg = (360 * box.dataset.percent / 100) + 180;
+        if(box.dataset.percent >= 50){
+            box.classList.add('over_50');
+        }else{
+            box.classList.remove('over_50');
+        }
+        box.querySelector('.piece.right').style.transform = 'rotate('+deg+'deg)';
+        box.querySelector('.text b').innerHTML=box.dataset.percent;
+    });
+}
+progressView();
+
+function scale() {
+    this.classList.toggle('big');
+}
+
+let scales=document.querySelectorAll('.scale');
+for (let i = 0; i < scales.length; i++) {
+    scales[i].addEventListener('click',scale);
+    
+}
